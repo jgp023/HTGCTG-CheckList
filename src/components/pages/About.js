054e6a94bdda8aab5f25f2632be57de163ab8c0e
Jordan Page <jordan_g.o.a.t.@Jordans-MacBook-Pro.local>
2020-04-18 21:34:47 -0500

    First Commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>
+        This is the "How To Get Closer to God Check List app v1.0.0. Here, users
+        will learn how to grow closer to God in the form of a daily check list
+      </p>
+    </React.Fragment>
+  );
+}
